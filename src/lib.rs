52c159e0866b5d505b8a64f69ad0f45f353aab62
James <james@prestwi.ch>
2022-12-02 13:36:39 -0500

    docs: slight update to lib docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,33 @@
 
 //! Utilities for long-running, resilient tasks.
 //!
-//! This library contains code I wrote, found useful, and want to keep using. It aims to provide systems
+//! This library contains code I wrote, found useful, and want to keep using.
+//! It aims to provide quick and re-usable scaffolding for building daemons.
+//! It is heavily optimized for my preferred dev ergonomics, and is not heavily
+//! optimized for performance.
 //!
-//! The general idiom is focused on spawning long-lived worker loops, which use channels to communicate.
+//! The general style is focused on spawning long-lived worker loops, which use
+//! channels to communicate.
 //!
 //! ```no_compile
-//! tokio::spawn(
-//!   async move {
-//!     while let Some(item) = pipe.next().await {
-//!       // work happens here :)
-//!     }
+//! impl Boulder for MyWorkerTask {
+//!   fn spawn(self) -> JoinHandle<Fall<Self>> {
+//!     tokio::spawn(async move {
+//!       while let Some(item) = self.pipe.next().await {
+//!         // work happens here :)
+//!       }
+//!     });
 //!   }
-//! );
+//! }
+//!
+//! let task = MyWorkerTask::new().run_forever();
 //! ```
 
 /// Pipe with process-once semantics
 pub mod pipe;
 /// Resumable, never-ending, tasks
 pub mod sisyphus;
-/// Crate utils
+/// Crate-internal utils
 mod utils;
 
 pub use pipe::{Pipe, PipeError};
